95d2faf25ed6afd0e56d0c464d57c55bdb5b9ed1
pablo609 <bmw602@o2.pl>
2016-06-27 16:38:05 +0200

    v

--- regex.sh
+++ regex.sh
@@ -2,8 +2,30 @@
 
 file=$1
 
-if ! [-r "$file" ]
+if ! [ -r "$file" ]
 then
 	echo "Cannot read the file!"
+    exit 0
 fi
 
+cat $file | sed 's/ /\n/g' > temp.txt
+
+cat temp.txt | sort > temp2.txt
+
+currentWord=""
+firstWord=1
+for word in `cat temp2.txt`
+do
+    if [ "$currentWord" = "$word" ]
+    then
+        wordCount=`expr $wordCount + 1`
+    else
+        if [ $firstWord -ne 1 ]
+        then
+            echo "$currentWord - $wordCount"
+        fi
+        currentWord=$word
+        wordCount=1
+        firstWord=0
+    fi
+done
